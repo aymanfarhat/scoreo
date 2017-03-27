a7c93970f340095f69e1316e04dcec6196edd7ee
Ayman <ayman.farhat1@gmail.com>
2017-03-27 14:50:03 +0300

    Define basic data model and test db create command

--- scoreo/commands.py
+++ scoreo/commands.py
@@ -1,7 +1,10 @@
 from scoreo import app
+from scoreo import db
+from scoreo import models
 
 
 @app.cli.command('initdb')
 def initdb_command():
-    """Creates the database tables."""
+    """Initializes the database tables"""
+    db.create_all()
     print('Initialized the database.')
--- scoreo/models.py
+++ scoreo/models.py
@@ -1,14 +1,54 @@
+import uuid
+from datetime import datetime
 from scoreo import db
 
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
+    fb_id = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, username, email):
-        self.username = username
+    def __init__(self, fb_id, email):
+        self.fb_id = fb_id
         self.email = email
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Player %r>' % self.slug
+
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, value):
+        self.value = value
+
+    def __repr__(self):
+        return '<Score %r>' % self.value
+
+
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, slug):
+        self.slug = slug
+
+    def __repr__(self):
+        return '<Board %r>' % self.slug
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    slug = db.Column(db.String(80), unique=True)
+    secret = db.Column(db.String(80), unique=True, default=str(uuid.uuid1()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, slug):
+        self.slug = slug
+
+    def __repr__(self):
+        return '<Game %r>' % self.slug
