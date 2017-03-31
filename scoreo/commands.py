9fd3441996f868501335cafa37740fdc23dcaf76
Ayman <ayman.farhat1@gmail.com>
2017-03-31 10:32:50 +0300

    Add a game create command that generates a secret

--- scoreo/commands.py
+++ scoreo/commands.py
@@ -7,3 +7,12 @@ def initdb_command():
     """Initializes the database tables"""
     models.db.create_all()
     print('Initialized the database.')
+
+@app.cli.command('initgame')
+def initgame_command():
+    """Initializes a new game with secret
+    or gets a matching one if same slug"""
+
+    slug = input('Select game slug: ') 
+    game = models.Game.first_or_create(slug)
+    print('Your game secret is: ' + game.secret)
